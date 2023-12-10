7b7368b5bd0bda2cdd4262f9c177dcad6571799c
ToXIc-Dev <info@samsstills.co.uk>
2023-12-10 20:38:25 +0000

    Update loud-beeper-gui.py

--- loud-beeper-gui.py
+++ loud-beeper-gui.py
@@ -19,7 +19,7 @@ user32.ShowWindow(hWnd, SW_HIDE)
 window = Tk()
 
 window.title("Loud Beeper")
-window.geometry('350x120')
+window.geometry('300x80')
 
 if(os.path.exists('data.json')):
     f = open('data.json')
@@ -27,15 +27,15 @@ if(os.path.exists('data.json')):
     threshold = data['threshold']
 else:
     threshold = 45
+    
+varLE = StringVar()
 
-lbl = Label(window, text="Threshold: " + str(threshold), font=("Arial Bold", 25))
-lbl.grid(column=0, row=0)
-var =IntVar()
+lbl = Label(window, text="Threshold: " + str(threshold), font=("Arial Bold", 15))
+lbl.grid(column=0, row=0, sticky = W, pady = 2)
+var = IntVar()
 var.set(threshold)
-spin = Spinbox(window, from_=0, to=100, width=5, textvariable=var, font=("Arial Bold", 25))
-spin.grid(column=0,row=1)
-lbl2 = Label(window, text="Last Triggered: ", font=("Arial Bold", 15))
-lbl2.grid(column=0, row=2)
+spin = Spinbox(window, from_=0, to=100, width=5, textvariable=var, font=("Arial Bold", 15))
+spin.grid(column=1,row=0, sticky = W, pady = 2)
 def clicked():
     global threshold
     threshold = int(spin.get())
@@ -43,7 +43,12 @@ def clicked():
     lbl.configure(text= res)
 
 btn = Button(window, text="Set", command=clicked, font=("Arial Bold", 15))
-btn.grid(column=1, row=1)
+btn.grid(column=2, row=0, sticky = W, pady = 2)
+
+lbl2 = Label(window, text="Last Exceeded: ", font=("Arial Bold", 15))
+lbl2.grid(column=0, row=1, sticky = W, pady = 2)
+lbl3 = Label(window, textvariable=varLE, font=("Arial Bold", 15))
+lbl3.grid(column=1, row=1, sticky = W, pady = 2)
 
 def print_sound(indata, frames, time, status):
     volume_norm = np.linalg.norm(indata)*10
@@ -54,8 +59,9 @@ def print_sound(indata, frames, time, status):
         pygame.mixer.init()
         pygame.mixer.music.load('beep.wav')
         pygame.mixer.music.play()
-        pygame.mixer.music.set_volume(0.1)   
-        lbl2.configure(text="Last Triggered: " + dt_string)        
+        pygame.mixer.music.set_volume(0.1)
+        varLE.set(dt_string)
+        window.update_idletasks()
     #print (int(volume_norm))
 
 stream = sd.InputStream(channels=1, callback=print_sound, blocksize=0, samplerate=4000)
